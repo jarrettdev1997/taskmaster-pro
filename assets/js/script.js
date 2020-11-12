c81edb6cdb92d7b094c5c17bbf63271355cc46c1
Jarrett Devose <jarrett.dev1997@gmail.com>
2020-11-12 17:58:14 -0500

    custom fonts/icons added

--- assets/js/script.js
+++ assets/js/script.js
@@ -282,5 +282,12 @@ var auditTask = function(taskEl) {
   else if (Math.abs(moment().diff(time, "days")) <= 2) {
     $(taskEl).addClass("list-group-item-warning");
   }
+  //console.log(taskEl);
 
 };
+
+setInterval(function () {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  });
+}, (1000 * 60) * 30);
